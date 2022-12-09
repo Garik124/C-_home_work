3bb28a9ed458fb2252c501c9dc01103d4bc0ca16
Garik <requie@mail.ru>
2022-12-09 12:41:07 +0700

    decided Task3 for Seminar 2

---
+++ c#_hw2/Task3/Program.cs
@@ -0,0 +1,20 @@
+﻿// Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным
+
+using static System.Console;
+Clear();
+
+void appointmentDay(int day)
+{
+    if (day == 6 || day == 7)
+    WriteLine ("выходной");
+    else if (day < 1 || day > 7)
+    WriteLine ("это не день недели");
+    else
+    WriteLine ("будний день");
+}
+
+Write("Введи цифру, обозначающую день недели: ");
+int dayNumber = int.Parse(ReadLine());
+
+appointmentDay(dayNumber);
+WriteLine();
