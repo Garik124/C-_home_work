1aa6ac77c701f96b36cc0dd6132727e526ec8d89
Garik <requie@mail.ru>
2022-12-15 22:51:40 +0700

    did task1

---
+++ c#_hw4/Task1/Program.cs
@@ -0,0 +1,25 @@
+﻿// Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+// int ReadFromConsole (string message)
+// {
+//     System.Console.WriteLine(message);
+//     return int.Parse(Console.ReadLine());
+// }
+
+double ApowB (double A, double B)
+{
+    double res = 0;
+    if (A > 0)
+    {
+        res = Math.Pow(A, B);
+        return res;
+    }
+    else
+    Console.WriteLine ("fail");
+    return res;
+}
+
+double result = ApowB (2, 4);
+Console.WriteLine(result);
