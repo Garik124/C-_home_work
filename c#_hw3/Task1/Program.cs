07806abedcc1b7da1cef054aae1e7b20e92a0376
Garik <requie@mail.ru>
2022-12-19 18:19:42 +0700

    hw3 task 1 changed

--- c#_hw3/Task1/Program.cs
+++ c#_hw3/Task1/Program.cs
@@ -6,9 +6,20 @@ Clear();
 WriteLine ("Введите число: ");
 String N = ReadLine();
 
-if (N[0]==N[4] || N[1]==N[3])
+// if (N[0]==N[4] || N[1]==N[3])                              //ЭТО ПЕРВОЕ РЕШЕНИЕ, НО МОЖНО РЕШИТЬ ЧРЕЗЕ bool НИЖЕ
+//     Console.WriteLine($"Ваше число: {N} - палиндром.");
+
+// else 
+//     Console.WriteLine($"Ваше число: {N} - НЕ палиндром.");
+
+bool NumberIsPalindrom (string number)
+{
+    return (number[0]==number[4] && number[1]==number[3]);
+}
+
+
+if (NumberIsPalindrom(N))                              //РЕШЕНИЕ С ПОМОЩЬЮ МЕТОДА bool
     Console.WriteLine($"Ваше число: {N} - палиндром.");
 
 else 
     Console.WriteLine($"Ваше число: {N} - НЕ палиндром.");
-
