adc12eb897e0c9b610e35a039c913493aab5e69e
Garik <requie@mail.ru>
2022-12-20 23:37:34 +0700

    hw7 task 3 complited

---
+++ c#_hw7/Task3/Program.cs
@@ -0,0 +1,64 @@
+﻿// Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+
+using static System.Console;
+Clear();
+
+int GetNumber(string message)
+{
+    WriteLine(message);
+    return int.Parse(ReadLine());
+}
+
+int[,] GetArray(int m, int n, int minValue, int maxValue)
+{
+    int[,] result = new int[m, n];
+
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+
+            result[i, j] = new Random().Next(minValue, maxValue + 1);
+        }
+    }
+
+    return result;
+}
+
+void PrintArray(int[,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+
+            Write($"\t{inArray[i, j]} ");
+        }
+        WriteLine();
+    }
+}
+
+void AverageColumnsMassive(int[,] numbers)
+{
+    for (int j = 0; j < numbers.GetLength(1); j++)
+    {
+        double avarage = 0;
+        for (int i = 0; i < numbers.GetLength(0); i++)
+        {
+            avarage = (avarage + numbers[i, j]);
+        }
+        avarage = avarage / numbers.GetLength(0);
+        Console.Write(avarage + "; ");
+    }
+}
+
+int rows = GetNumber("Введите количество строк: ");
+int cols = GetNumber("Введите количество столбцов: ");
+int[,] array = GetArray(rows, cols, 0, 10);
+PrintArray(array);
+AverageColumnsMassive(array);
